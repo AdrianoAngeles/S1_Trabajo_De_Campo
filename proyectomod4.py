ce4e1720836ca2a8102d16801956d08b4240ba55
catherine <catherinerv.suga@gmail.com>
2025-09-12 02:24:46 -0500

    tercer y cuarto boton

---
+++ proyectomod4.py
@@ -0,0 +1,40 @@
+print ("Iniciando Proyecto")
+print ("GRUPO: 4")
+print ("Integrantes")
+print ("Rodrigo Moises Ismael Portugal Davila")
+print ("Catherine Lucero Ramos Vasquez")
+print ("Joaquin David Sandoval Garcia")
+print ("Jesus Alberto Yovera Vergara")
+print ("Adriano Stefano Angeles Miñano")
+print ("Primer Boton")
+#" String codigo = txtCodigo.getText()
+#		    String nombre = txtNombre.getText();
+#		    String categoria = cdCategoría.getSelectedItem().toString();
+#		    String precio = txtPrecio.getText();
+#		    String stock = txtStock.getText();
+#		    txtS.append("Código: " + codigo 
+#		              + ", Nombre: " + nombre 
+#		              + ", Categoría: " + categoria 
+#		              + ", Precio: $" + precio 
+#		              + ", Stock: " + stock + "\n");
+#		    txtS.append(">> Producto agregado correctamente\n\n");")
+print ("segundo  boton")
+# protected void do_btnNewButton_1_actionPerformed(ActionEvent e) {
+#	    String codigoBuscado = txtCodigo.getText();
+#	    String texto = txtS.getText(); 
+#	    if (texto.contains(codigoBuscado)) {
+#	        txtS.append(">> Producto con código " + codigoBuscado + " encontrado\n");
+#	    } else {
+#	        txtS.append(">> Producto con código " + codigoBuscado + " no encontrado\n");
+#	    }	
+#	}      
+print("tercer boton ")
+#protected void do_limpiar_actionPerformed(ActionEvent e) {
+#		  txtCodigo.setText("");
+#		    txtNombre.setText("");
+#		    txtPrecio.setText("");
+#		    txtStock.setText("");
+#		    cdCategoría.setSelectedIndex(0);  
+#		    txtCodigo.requestFocus();       
+print ("cuarto boton")
+#System.exit(0);
